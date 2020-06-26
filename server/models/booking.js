ed1bcdd1af953ff06464757f5deb3b3c19b19e46
Siraj Alam <Sirajalam049@gmail.com>
2020-06-27 03:07:13 +0530

    Booking params made optional for saving top draft

--- server/models/booking.js
+++ server/models/booking.js
@@ -1,5 +1,19 @@
 'use strict';
 
-module.exports = function(Booking) {
+const { BAD_REQUEST } = require("http-status-codes");
 
+module.exports = function (Booking) {
+    Booking.beforeValidate = function (next, modelInstance) {
+        const { inDraft, numberOfNights, numberOfRooms, date } = modelInstance;
+        console.log({ modelInstance });
+        if (!date) throw { statusCode: BAD_REQUEST, message: "Date cannot be empty" };
+        if (modelInstance.inDraft) {
+            next();
+        }
+        else {
+            if (!numberOfNights) throw { statusCode: BAD_REQUEST, message: "Number of nights cannot be empty or zero" };
+            if (!numberOfRooms) throw { statusCode: BAD_REQUEST, message: "Number of rooms cannot be empty or zero" };
+            next();
+        }
+    };
 };
